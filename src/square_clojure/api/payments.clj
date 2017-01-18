16695468f5a3c092c102b7e509f64a2d182f1539
mark nery <marknery@gmail.com>
2017-01-18 13:11:39 -0700

    fix payments endpoint

--- src/square_clojure/api/payments.clj
+++ src/square_clojure/api/payments.clj
@@ -5,12 +5,14 @@
 
 (defn daily-sales [date location-id offset token]
   (let [offset-datetime (t/from-time-zone date (t/time-zone-for-offset -7))]
-    (square-get (str location-id
-                     "/payments?begin_time="
-                     (c/to-string offset-datetime)
-                     "&end_time="
-                     (c/to-string (t/plus offset-datetime (t/days 1)))
-                     "&limit=200") token )))
+    (square-get (str
+                 "/"
+                 location-id
+                 "/payments?begin_time="
+                 (c/to-string offset-datetime)
+                 "&end_time="
+                 (c/to-string (t/plus offset-datetime (t/days 1)))
+                 "&limit=200") token )))
 
 (defn daily-tips [date location-id offset token]
   (reduce + (map (fn [x] (get-in x ["tip_money" "amount"]))
